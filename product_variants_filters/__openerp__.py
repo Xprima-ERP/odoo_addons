321d36370102c5b92f92eafde91c8a7e666c440a
Sebastien Delisle <seb0del@gmail.com>
2015-04-15 08:43:52 -0400

    product_variants_filters module. init commit

---
+++ product_variants_filters/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Variants Filters",
+
+    'summary': """
+    Filters product variants to produce only logical combinations
+    """,
+
+    'description': """
+       Given a list of logical combinations, filter out non logical combinations
+    of products variants
+    """,
+
+    'author': "Xprima",
+    'website': "http://www.xprima.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
---
+++ product_variants_filters/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class product_variants_filters(models.Model):
+#     _name = 'product_variants_filters.product_variants_filters'
+
+#     name = fields.Char()
