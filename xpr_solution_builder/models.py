acdb92ebf4a6c47076948d572a000379a484e9b7
Charles De Lean <cdelean@xprima.com>
2015-06-11 11:58:22 -0400

    Update of order based on selection in wizard

--- xpr_solution_builder/models.py
+++ xpr_solution_builder/models.py
@@ -105,6 +105,21 @@ class SolutionConfigurator(models.TransientModel):
     def _default_solution(self):
         return self.env['xpr_solution_builder.solution'].browse(self._context.get('solution_id'))
 
+    def _default_products(self):
+        order = self._default_order()
+        solution = self._default_solution()
+
+        if not order or not solution:
+            # Should not get here.
+            return []
+
+        # Return optional products that are at least one order line
+
+        options = set([item.id for item in solution.options])
+        products = set([line.product_id.id for line in order.order_line])
+
+        return sorted(options & products)
+
     order = fields.Many2one(
         'sale.order',
         string='Order',
@@ -120,14 +135,45 @@ class SolutionConfigurator(models.TransientModel):
     products = fields.Many2many(
         'product.product',
         'xpr_solution_builder_solution_configurator_product_rel',
-        string='Product')
+        string='Product',
+        default=_default_products)
 
     @api.one
     def set_products(self):
-       
+
+        options = set([item.id for item in self.solution.options])
+
+        selected_products = set([product.id for product in self.products])
+        products_in_order = set()
+
+        removed_lines = []
+
+        for line in self.order.order_line:
+            if line.product_id.id not in options:
+                continue
+
+            products_in_order.add(line.product_id.id)
+
+            if line.product_id.id in selected_products:
+                continue
+
+            # Optional prouct not selected anymore.
+            # Delete line from order.
+            removed_lines.append(line)
+
+        # Delete lines of optinal products that are not selected anymore
+        for line in removed_lines:
+            line.unlink()
+
         line = self.env['sale.order.line']
 
+        added_products = selected_products - products_in_order
+
+        # Go through products again and insert lines for newly selected ones.
         for product in self.products:
+            if product.id not in added_products:
+                continue
+
             line.create(
                 dict(
                     order_id=self.order.id,
@@ -138,14 +184,14 @@ class SolutionConfigurator(models.TransientModel):
         return {}
 
     @api.one
-    @api.onchange('solution_id')
+    @api.onchange('solution')
     def onchange_solution(self):
-
-        domain_ids = []
 
         solution = self.solution
 
-        if solution:
-            domain_ids = [item.id for item in solution.options]
+        if not solution:
+            return []
+
+        domain_ids = [item.id for item in solution.options]
 
         return {'domain': {'products': [('id', 'in', domain_ids)]}}
