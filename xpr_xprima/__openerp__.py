41a7895978ce8318c209ce8334c7c6383780d3af
Charles De Lean <cdelean@xprima.com>
2015-07-02 15:03:56 -0400

    Removed dependency from warehouse management

---
+++ xpr_xprima/__openerp__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+{'name': 'Xprima modules',
+ 'version': '1.0',
+ 'summary': 'Xprima Module Dependencies',
+ 'description': """Contains all the Xprima dependencies
+      Including its own cosum modules:
+      - Sales process
+      - Reports
+ """,
+ 'category': 'Misc',
+ 'author': 'Charles De Lean',
+ 'website': 'http://xprima.com',
+ 'license': 'AGPL-3',
+ 'depends': [
+      'hr',
+      'crm',
+      'sale',
+      'xpr_solution_builder',
+      'xpr_sale_process',
+      'xpr_product_variant_filters',
+      'agaplan_terms_and_conditions',
+      'xpr_report',
+
+      # From 7.0 version
+
+      # 'project',
+      # 'purchase',
+      # 'canadian_provinces',
+
+      # 'partner_history',
+      # 'product_custom_attributes',
+      # 'web_adblock',
+      # 'mass_editing',
+      # 'audittrail',
+      # 'xprima_product',
+      #'xprima_sale_order',
+
+      #'sale_package_configurator',
+      #'so_second_level_approval_and_discount',
+      #'xprima_report',
+
+      # 'xis_connector',
+      # 'csv_noupdate',
+      # 'xpr_hd_ticket',
+      # 'no_customer_auto_follow',
+      # 'xpr_seo',
+      # 'xpr_csi_program',
+      # 'xpr_casa',
+      # 'hr_employee_auto_create',
+      # 'reps_nocreate_dealer',
+            ],
+ 'data': [],
+ 'demo': [],
+ 'installable': True,
+ # 'auto_install': True,
+ # 'application': False,
+}
