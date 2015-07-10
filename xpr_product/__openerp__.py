ac5540d0398a98d093e9bf4a914262e5d0d0e1a9
Charles De Lean <cdelean@xprima.com>
2015-07-10 16:03:20 -0400

    Xprima Product version 7.0

---
+++ xpr_product/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{'name': 'Xprima Product',
+ 'version': '0.001',
+ 'summary': 'Xprima Product',
+ 'description': 'Xprima Product',
+ 'category': 'misc',
+ 'author': 'Sebastien Delisle',
+ 'website': 'www.xprima.com',
+ 'depends': [
+             'product',
+             'contract_isp',
+ ],
+ 'data': [
+     'product_view.xml',
+ ],
+ 'installable': True,
+ 'auto_install': False,
+ 'application': False,
+ }
