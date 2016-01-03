13324262604978929415b3840a35d8b82bec5aca
Charles De Lean <cdelean@xprima.com>
2016-01-02 22:37:08 -0500

    Initial Project module commit

---
+++ xpr_project/__openerp__.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Xprima Project',
+    'version': '0.1',
+    'author': 'Xprima',
+    'maintainer': 'Xprima',
+    'website': 'http://www.xprima.com',
+    'license': 'AGPL-3',
+    'category': 'Sale',
+    'summary': 'Sync project management with Xprima',
+    'description': """
+Xprima JIRA Connector
+=====================
+
+Adapts project management to Xprimas needs:
+
+- Synchs with JIRA for production needs
+- Gives high level project status.
+
+Contributors
+------------
+* Charles De Lean <cdelean@xprima.com>
+""",
+    'depends': [
+        'base',
+        #'project',
+    ],
+    'external_dependencies': {
+        #'python': ['jira'],
+    },
+    'data': [
+        #'config.xml',
+        # 'views/res_user.xml',
+        # 'views/res_partner_categories.xml',
+        # 'res_partners_categories.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ xpr_project/jira_request.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python2
+# -*- encoding: utf-8 -*-
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+CONFIG_KEY_ENABLE = 'jira.enable'
+CONFIG_KEY_URL = 'jira.url'
+CONFIG_KEY_PRODUCTION_PROJECT = 'jira.production.project'
+
+
+class JIRARequest(object):
+
+    _jira = None
+    _config_params = None
+
+    """
+    Class to send request to XIS software. Using POST http request.
+    """
+
+    def __init__(self, model_instance):
+        self.model_instance = model_instance
+
+    def get_config(self, key):
+
+        if not self._config_params:
+            self._config_params = model_instance.env['ir.config_parameter']
+
+        lst_param = self._config_params.search([
+            ('key', '=', key)])
+
+        if not lst_param:
+            return None
+
+        return lst_param[0].value
+
+    @property
+    def jira(self):
+
+        if not self._jira:
+
+            enable = (self.get_config(CONFIG_KEY_ENABLE) or '').lower()
+
+            if enable != '1' and enable != 'true':
+                _logger.info("jira.enable is not set.")
+                return None
+
+            from jira import JIRA
+
+            self._jira = JIRA(
+                #basic_auth=('user', 'userpassword'), #TODO Fill in a user here
+                server=self.get_config(CONFIG_KEY_URL))
+
+        return self._jira
+
+    def execute(self):
+        """
+        Main entry point to execute the action
+        Manages exceptions
+        """
+        try:
+            return self.safe_execute()
+        except Exception as e:
+            _logger.error("jira connector error: {0}".format(e))
+            raise
+
+    def safe_execute(self):
+        """
+        Override this method in subclasses to implement
+        """
+        return None
+
+
+class CreateIssueRequest(JIRARequest):
+
+    def get_project(self):
+        """
+        Override this function to provide the JIRA project name
+        """
+
+        return None
+
+    def safe_execute(self):
+
+        i = self.jira.create_issue(
+            fields=dict(
+                project={'key': self.get_project()},
+                summary='Remote test',
+                description='This is a test',
+                issuetype={'name': 'Story'}))
+
+        #jira.add_attachment(i.key, "~/Desktop/deleteme")
+
+        #jira.add_comment(i.key, "My comment")
+
+
+class CreateProduction(CreateIssueRequest):
+
+    def get_project(self):
+        """
+        Override this function to provide the JIRA project name
+        """
+
+        return self.get_config(CONFIG_KEY_PRODUCTION_PROJECT)
