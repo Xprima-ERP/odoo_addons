610bb417d1d2c14c51807ac08c6fa426781792e5
Charles De Lean <cdelean@xprima.com>
2016-01-04 21:18:14 -0500

    373 - Disabling CSI account manager

--- xpr_csi_program/models.py
+++ xpr_csi_program/models.py
@@ -4,11 +4,11 @@ from openerp import models, fields, api
 from openerp.tools.translate import _
 
 
-class Partner(models.Model):
-    #_name = 'res.partner'
-    _inherit = 'res.partner'
+# class Partner(models.Model):
+#     #_name = 'res.partner'
+#     _inherit = 'res.partner'
 
-    csi_contact_am = fields.Many2one('res.partner', 'CSI Contact A.M.')
+#     csi_contact_am = fields.Many2one('res.partner', 'CSI Contact A.M.')
 
 
 class SaleOrder(models.Model):
@@ -16,8 +16,3 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     csi_contact = fields.Many2one('res.partner', 'CSI Contact Sales')
-
-    # Form helper
-
-    def on_change_partner_id(self, cr, uid, ids, partner_id, context):
-        return {'domain': {'csi_contact': [('parent_id', '=', partner_id)]}}
