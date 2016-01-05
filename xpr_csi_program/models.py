9fad17f76d0fabc8ff70d06984ac90ba13dc04c4
Charles De Lean <cdelean@xprima.com>
2016-01-04 22:10:57 -0500

    890 - Set CSI partner for web site contracts only

--- xpr_csi_program/models.py
+++ xpr_csi_program/models.py
@@ -15,4 +15,12 @@ class SaleOrder(models.Model):
     #_name = 'sale.order'
     _inherit = 'sale.order'
 
+    @api.onchange('partner_id')
+    def _change_csi_contact_domain(self):
+        return {'domain': {'csi_contact': [('parent_id', '=', self.partner_id)]}}
+
+    @api.onchange('solution')
+    def _reset_csi_contact(self):
+        self.csi_contact = None
+
     csi_contact = fields.Many2one('res.partner', 'CSI Contact Sales')
